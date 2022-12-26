58a37e8439994831c5d275a403878ad9ddf5dc99
Q.s <wangqs_eclipse@yahoo.com>
2022-12-26 17:20:48 +0800

    chore: update to the right tag

--- scripts/build_harbor.sh
+++ scripts/build_harbor.sh
@@ -4,7 +4,7 @@ export BUILD_TIME=$(date +'%Y-%m-%dT%H:%M:%S%z')
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 docker buildx build -f Dockerfile_github \
-	-t jeepay-ui-payment:latest \
+	-t oci.tuxm.art:8443/tusdesign/jeepay-ui-payment:latest \
     --build-arg USER=${GITHUB_USER} \
     --build-arg TOKEN=${GITHUB_TOKEN} \
     --build-arg PLATFORM=payment \
@@ -13,14 +13,14 @@ docker buildx build -f Dockerfile_github \
     --build-arg COMMIT_SHA=${COMMIT_SHA} .
 
 docker buildx build -f Dockerfile_github \
- -t jeepay-ui-manager:latest \
+ -t oci.tuxm.art:8443/tusdesign/jeepay-ui-manager:latest \
  --build-arg PLATFORM=manager \
  --build-arg BUILD_TIME=${BUILD_TIME} \
  --build-arg VERSION=${VERSION} \
  --build-arg COMMIT_SHA=${COMMIT_SHA} .
 
 docker buildx build -f Dockerfile_github \
- -t jeepay-ui-merchant:latest \
+ -t oci.tuxm.art:8443/tusdesign/jeepay-ui-merchant:latest \
  --build-arg PLATFORM=merchant \
  --build-arg BUILD_TIME=${BUILD_TIME} \
  --build-arg VERSION=${VERSION} \
