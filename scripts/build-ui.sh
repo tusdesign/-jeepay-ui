34375268285bb6515f1e7656fdbfb7cf8c4720f4
Q.s <wangqs_eclipse@yahoo.com>
2023-01-01 15:26:28 +0800

    chore: update script to push to private repo

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -5,3 +5,8 @@ docker buildx build . --build-arg PLATFORM=cashier -t jeepay-ui-cashier:latest
 docker buildx build . --build-arg PLATFORM=manager -t jeepay-ui-manager:latest
 
 docker buildx build . --build-arg PLATFORM=merchant -t jeepay-ui-merchant:latest
+
+docker login oci.tuxm.art:8443 --username ${HARBOR_USER} --password ${HARBOR_PASS}
+docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-payment:latest
+docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-manager:latest
+docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-merchant:latest
