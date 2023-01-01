8adeced87d4a7de5d411b0e12d08f21864655af0
Q.s <wangqs_eclipse@yahoo.com>
2023-01-01 16:43:56 +0800

    code cleanup

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -1,9 +1,7 @@
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 docker buildx build . --build-arg PLATFORM=cashier -t jeepay-ui-cashier:latest
-
 docker buildx build . --build-arg PLATFORM=manager -t jeepay-ui-manager:latest
-
 docker buildx build . --build-arg PLATFORM=merchant -t jeepay-ui-merchant:latest
 
 docker login oci.tuxm.art:8443 --username ${HARBOR_USER} --password ${HARBOR_PASS}
