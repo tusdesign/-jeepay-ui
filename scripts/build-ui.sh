c9093c319abc90947c6406e49f4f623bb1955281
Q.s <wangqs_eclipse@yahoo.com>
2023-07-15 18:21:59 +0800

    parameterize the scripts

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -1,10 +1,10 @@
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
-docker buildx build . --build-arg PLATFORM=cashier -t jeepay-ui-cashier:latest
-docker buildx build . --build-arg PLATFORM=manager -t jeepay-ui-manager:latest
-docker buildx build . --build-arg PLATFORM=merchant -t jeepay-ui-merchant:latest
+docker buildx build . --build-arg PLATFORM=cashier -t jeepay-ui-cashier:$TAG
+docker buildx build . --build-arg PLATFORM=manager -t jeepay-ui-manager:$TAG
+docker buildx build . --build-arg PLATFORM=merchant -t jeepay-ui-merchant:$TAG
 
-docker login oci.tuxm.art:8443 --username ${HARBOR_USER} --password ${HARBOR_PASS}
-docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-payment:latest
-docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-manager:latest
-docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-merchant:latest
+docker login $HARBOR_URL --username ${HARBOR_USER} --password ${HARBOR_PASS}
+docker push $HARBOR_URL/tusdesign/jeepay-ui-payment:$TAG
+docker push $HARBOR_URL/tusdesign/jeepay-ui-manager:$TAG
+docker push $HARBOR_URL/tusdesign/jeepay-ui-merchant:$TAG
--- scripts/build_harbor.sh
+++ scripts/build_harbor.sh
@@ -3,31 +3,32 @@ export BUILD_TIME=$(date +'%Y-%m-%dT%H:%M:%S%z')
 
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
-docker buildx build -f Dockerfile_github \
-	-t oci.tuxm.art:8443/tusdesign/jeepay-ui-payment:latest \
-    --build-arg USER=${GITHUB_USER} \
-    --build-arg TOKEN=${GITHUB_TOKEN} \
-    --build-arg PLATFORM=cashier \
-    --build-arg BUILD_TIME=${BUILD_TIME} \
-    --build-arg VERSION=${VERSION} \
-    --build-arg COMMIT_SHA=${COMMIT_SHA} .
+# disable cashier for saving some time. We don't need it. 
+# docker buildx build -f Dockerfile_github \
+# 	-t $HARBOR_URL/tusdesign/jeepay-ui-payment:$TAG \
+#     --build-arg USER=${GITHUB_USER} \
+#     --build-arg TOKEN=${GITHUB_TOKEN} \
+#     --build-arg PLATFORM=cashier \
+#     --build-arg BUILD_TIME=${BUILD_TIME} \
+#     --build-arg VERSION=${VERSION} \
+#     --build-arg COMMIT_SHA=${COMMIT_SHA} .
 
 docker buildx build -f Dockerfile_github \
- -t oci.tuxm.art:8443/tusdesign/jeepay-ui-manager:latest \
+ -t $HARBOR_URL/tusdesign/jeepay-ui-manager:$TAG \
  --build-arg PLATFORM=manager \
  --build-arg BUILD_TIME=${BUILD_TIME} \
  --build-arg VERSION=${VERSION} \
  --build-arg COMMIT_SHA=${COMMIT_SHA} .
 
 docker buildx build -f Dockerfile_github \
- -t oci.tuxm.art:8443/tusdesign/jeepay-ui-merchant:latest \
+ -t $HARBOR_URL/tusdesign/jeepay-ui-merchant:$TAG \
  --build-arg PLATFORM=merchant \
  --build-arg BUILD_TIME=${BUILD_TIME} \
  --build-arg VERSION=${VERSION} \
  --build-arg COMMIT_SHA=${COMMIT_SHA} .
 
-docker login oci.tuxm.art:8443 --username ${HARBOR_USER} --password ${HARBOR_PASS}
-docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-payment:latest
-docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-manager:latest
-docker push oci.tuxm.art:8443/tusdesign/jeepay-ui-merchant:latest
+docker login $HARBOR_URL --username ${HARBOR_USER} --password ${HARBOR_PASS}
+docker push $HARBOR_URL/tusdesign/jeepay-ui-payment:$TAG
+docker push $HARBOR_URL/tusdesign/jeepay-ui-manager:$TAG
+docker push $HARBOR_URL/tusdesign/jeepay-ui-merchant:$TAG
 
