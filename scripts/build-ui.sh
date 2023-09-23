7f1c3ae552ccedd88737d50f4a4be8f941f2fc9a
Q.S. Wang <wangqs_eclipse@yahoo.com>
2023-09-23 23:27:09 +1000

    chore: provide correct image path

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -8,9 +8,9 @@ if [ "$TAG" = "" ]; then
     TAG="latest"
 fi
 
-docker buildx build . --build-arg PLATFORM=cashier -t jeepay-ui-cashier:$TAG
-docker buildx build . --build-arg PLATFORM=manager -t jeepay-ui-manager:$TAG
-docker buildx build . --build-arg PLATFORM=merchant -t jeepay-ui-merchant:$TAG
+docker buildx build . --build-arg PLATFORM=cashier -t $HARBOR_URL/tusdesign/jeepay-ui-cashier:$TAG
+docker buildx build . --build-arg PLATFORM=manager -t $HARBOR_URL/tusdesign/jeepay-ui-manager:$TAG
+docker buildx build . --build-arg PLATFORM=merchant -t $HARBOR_URL/tusdesign/jeepay-ui-merchant:$TAG
 
 docker login $HARBOR_URL --username ${HARBOR_USER} --password ${HARBOR_PASS}
 docker push $HARBOR_URL/tusdesign/jeepay-ui-payment:$TAG
